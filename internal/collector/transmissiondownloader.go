3254b802fa3809d26c8daa38446a6b64497a7824
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-06-09 10:24:20 +0200

    add transmissiondownloader

---
+++ internal/collector/transmissiondownloader.go
@@ -0,0 +1,153 @@
+package collector
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+// TransmissionDownloader is a type that implements the Downloader interface to open a downloadable using xdg-open.
+type TransmissionDownloader struct {
+	Downloadable
+	Transmission *TransmissionConnection
+	client       *http.Client
+}
+
+type TransmissionConnection struct {
+	Host string
+	Port string
+	User string
+	Pass string
+	SSL  bool
+}
+
+type transmissionRequest struct {
+	Method    string                `json:"method"`
+	Arguments transmissionArguments `json:"arguments"`
+}
+
+type transmissionArguments struct {
+	Paused   bool   `json:"paused"`
+	Filename string `json:"filename"`
+}
+
+var (
+	re = regexp.MustCompile("<code>X-Transmission-Session-Id: (.*)</code>")
+)
+
+// NewTransmissionDownloader creates a new TransmissionDownloader
+func NewTransmissionDownloader(downloadable Downloadable, connection *TransmissionConnection) *TransmissionDownloader {
+	return &TransmissionDownloader{
+		Downloadable: downloadable,
+		Transmission: connection,
+		client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+// Download does an http post to a trasmission server to download the torrent file
+func (d *TransmissionDownloader) Download() error {
+	sessionID, err := d.getSessionID()
+	if err != nil {
+		return err
+	}
+
+	body, err := getBody(d.URI())
+	if err != nil {
+		return err
+	}
+
+	req, err := d.Transmission.newRequest()
+	if err != nil {
+		return err
+	}
+	req.Header.Set("X-Transmission-Session-Id", *sessionID)
+	req.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	resp, err := d.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func getBody(uri string) ([]byte, error) {
+	torrentAdd := transmissionRequest{
+		Method: "torrent-add",
+		Arguments: transmissionArguments{
+			Paused:   false,
+			Filename: uri,
+		},
+	}
+
+	jsonStr, err := json.Marshal(torrentAdd)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonStr, nil
+}
+
+func (d *TransmissionDownloader) getSessionID() (*string, error) {
+	req, err := d.Transmission.newRequest()
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := d.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	match := re.FindStringSubmatch(string(body))
+	if len(match) < 2 {
+		return nil, fmt.Errorf("no session id found")
+	}
+
+	return &match[1], nil
+}
+
+func (tc *TransmissionConnection) newRequest() (*http.Request, error) {
+	req, err := http.NewRequest(
+		http.MethodPost,
+		tc.transmissionURL(),
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(tc.User, tc.Pass)
+
+	return req, nil
+}
+
+func (tc *TransmissionConnection) transmissionURL() string {
+	protocol := "http"
+	if tc.SSL {
+		protocol = "https"
+	}
+
+	return fmt.Sprintf("%s://%s/transmission/rpc/",
+		protocol,
+		net.JoinHostPort(tc.Host, tc.Port),
+	)
+}
---
+++ internal/collector/transmissiondownloader_test.go
@@ -0,0 +1,24 @@
+package collector
+
+import (
+	"testing"
+)
+
+func TestTransmissionDownloader_Open(t *testing.T) {
+	t.Skip()
+
+	magnet := Magnet{
+		uri: "magnet:?xt=urn:btih:88594aaacbde40ef3e2510c47374ec0aa396c08e&dn=Big+Buck+Bunny+1080p+30fps&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.stealth.si%3A80&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337",
+	}
+	td := NewTransmissionDownloader(
+		magnet,
+		&TransmissionConnection{
+			Host: "localhost",
+			Port: "9091",
+			User: "transmission",
+			Pass: "",
+		},
+	)
+
+	td.Download()
+}
