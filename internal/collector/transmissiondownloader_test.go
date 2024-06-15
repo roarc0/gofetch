07914c9f887211bec976669d8e08deba31979551
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2024-06-15 14:05:05 +0200

    skip test is env vars are not set

--- internal/collector/transmissiondownloader_test.go
+++ internal/collector/transmissiondownloader_test.go
@@ -1,21 +1,28 @@
 package collector
 
 import (
+	"os"
 	"testing"
 )
 
 func TestTransmissionDownloader_Open(t *testing.T) {
-	t.Skip()
+	user := os.Getenv("TRANSMISSION_USER")
+	pass := os.Getenv("TRANSMISSION_PASS")
+
+	if user == "" || pass == "" {
+		t.Skip("TRANSMISSION_USER and TRANSMISSION_PASS are not set")
+	}
 
 	magnet := Magnet{
 		uri: "magnet:?xt=urn:btih:88594aaacbde40ef3e2510c47374ec0aa396c08e&dn=Big+Buck+Bunny+1080p+30fps&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.stealth.si%3A80&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337",
 	}
+
 	td := NewTransmissionDownloader(
 		&TransmissionConfig{
 			Host: "localhost",
 			Port: "9091",
-			User: "transmission",
-			Pass: "",
+			User: user,
+			Pass: pass,
 		},
 	)
 
